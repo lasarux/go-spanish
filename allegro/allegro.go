6fdd45a4b02a319082a7b6e20605a8fc73ffcdc6
LaSaRuX <lasarux@gmail.com>
2009-11-17 00:57:01 +0100

    Añadido un wrapper super básico para allegro

---
+++ allegro/allegro.go
@@ -0,0 +1,98 @@
+/*****************************************************************************
+ * Copyright (c) 2009, Pedro Gracia <lasarux@neuroomante.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * Neither the name of Pedro Gracia nor the names of his contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+package allegro
+
+/*
+#include <allegro.h>
+
+int a_init(void) {
+    return allegro_init();
+}
+
+void a_install_keyboard(void) {
+    install_keyboard(); 
+}
+
+int a_set_gfx_mode(void) {
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 400, 0, 0) != 0) {
+        if (set_gfx_mode(GFX_SAFE, 640, 400, 0, 0) != 0) {
+            set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
+            allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void a_textout_centre_ex(char *text) {
+    //set_palette(desktop_palette);
+    clear_to_color(screen, makecol(255, 255, 255));
+    acquire_screen();
+    textout_centre_ex(screen, font, text, SCREEN_W/2, SCREEN_H/2, makecol(0,0,0), -1);
+    release_screen();
+}
+
+void a_putpixel(int x, int y, int color) {
+    putpixel(screen, x, y, color);
+}
+
+void a_readkey(void) {
+    readkey();
+}
+
+*/
+import "C"
+//import "unsafe"
+
+func Init() {
+	C.a_init();
+}
+
+func Install_keyboard() {
+    C.a_install_keyboard();
+}
+
+func Set_gfx_mode() {
+    C.a_set_gfx_mode();
+}
+
+func Readkey() {
+    C.a_readkey();
+}
+
+func Textout_centr_ex(text string) {
+    cs := C.CString(text);
+    C.a_textout_centre_ex(cs);
+}
+    
+func Putpixel(x, y, color int) {
+    C.a_putpixel(_C_int(x), _C_int(y), _C_int(color));
+}
---
+++ allegro/allegro_test.go
@@ -0,0 +1,79 @@
+/*****************************************************************************
+ * Copyright (c) 2009, Pedro Gracia <lasarux@neuroomante.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * Neither the name of Pedro Gracia nor the names of his contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+package main
+
+import "./allegro"
+import "math"
+
+func main() {
+    // Init window
+    allegro.Init();
+    allegro.Install_keyboard();
+    allegro.Set_gfx_mode();
+    
+    // Show text centered
+    allegro.Textout_centr_ex("Hello, world!");
+    allegro.Readkey();
+    
+    // More text centered
+    allegro.Textout_centr_ex("Hola, mundo!");
+    allegro.Readkey();
+    
+    // Draw a mandelbrot set
+    a := 0.;
+    b := 0.;
+    aa := 0.;
+    bb := 0.;
+    ca := 0.;
+    cb := 0.;
+    i := 0;
+    max := 64;
+    
+    for y := 0; y<400; y++ {
+        for x := 0; x<640; x++ {
+            i = 0;
+            ca = (float) (x - 420)/200;
+			cb = (float) (y - 200)/200;
+			a = 0.;
+            b = 0.;
+            for i < max && math.Sqrt(float64(a*a)+float64(b*b)) <= 2 { 
+                aa = a*a - b*b + ca;
+				bb = 2*a*b + cb;
+				a = aa;
+				b = bb;
+				i++;
+            }
+            
+            allegro.Putpixel(x, y, i);
+        }
+    }
+    allegro.Readkey();
+}
